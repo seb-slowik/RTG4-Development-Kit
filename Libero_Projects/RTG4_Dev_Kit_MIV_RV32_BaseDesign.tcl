fb338f9922817d075411f4f05d7c8b5355f9c7a9
Sebastian Slowikowski <Seb.Slowikowski@microchip.com>
2023-06-30 11:06:44 +0100

    Legacy core smartdesign builder fixed, minor tweaks to software program references

--- Libero_Projects/RTG4_Dev_Kit_MIV_RV32_BaseDesign.tcl
+++ Libero_Projects/RTG4_Dev_Kit_MIV_RV32_BaseDesign.tcl
@@ -76,11 +76,6 @@ if {[file exists $projectDir] == 1} then {
 # Download the required direct cores
 #download_required_direct_cores "$hwPlatform" "$softCpu" "$config"
 
-# Copy the example software program into the project directory
-# # file copy -force $scriptDir/import/software_example/$cpuGroup/$config/hex $projectDir
-puts "Info: File copy hex: $scriptDir/import/software_example/$cpuGroup/hex to: $projectDir"
-file copy -force $scriptDir/import/software_example/$softCpu/hex $projectDir
-
 # Import and build the design's SmartDesign
 print_message "Building the $sdName..."
 source $scriptDir/import/build_smartdesign/$sdBuildScript
