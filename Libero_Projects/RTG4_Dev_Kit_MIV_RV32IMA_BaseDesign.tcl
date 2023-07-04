6993c58e282a7f5addd71fa8249588cc394d448b
Sebastian Slowikowski <Seb.Slowikowski@microchip.com>
2023-07-04 21:04:40 +0100

    Job file naming fix

--- Libero_Projects/RTG4_Dev_Kit_MIV_RV32IMA_BaseDesign.tcl
+++ Libero_Projects/RTG4_Dev_Kit_MIV_RV32IMA_BaseDesign.tcl
@@ -33,7 +33,7 @@ get_die_configuration $hwPlatform $dieType
 print_message "Runnig script: $scriptPath \nDesign Arguments: $config $designFlow $dieType \nDesign Build Script: $sdBuildScript"
 
 # Configure Libero project files and directories
-append projectName $hwPlatform _ $dieType _ $cpuRef _ $config _ $sdName
+set projectName "${hwPlatform}[expr {$dieType eq "ES" ? "_${dieType}" : ""}]_${cpuRef}_${config}_${sdName}" ; # projectName only reflects dieType if dieType is "ES"
 append projectFolderName "${cpuRef}_${config}_BD"
 set projectDir $scriptDir/$projectFolderName
 puts "Info: projectName: $projectName"
